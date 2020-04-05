68e9bd5d6835eab4116c1754a9b798e233fd41f0
Agustín Martínez <agustin@MacBook-Pro-de-Agustin.local>
2020-04-05 17:44:54 -0300

    Agrego getters, setters y constructor

--- Usuario.java
+++ Usuario.java
@@ -7,6 +7,65 @@ public class Usuario {
   Integer cantidad_de_hijos;
   Integer saldo;
 
+    public String getNombre() {
+        return nombre;
+    }
+
+    public Integer getCedula() {
+        return cedula;
+    }
+
+    public String getContrasena() {
+        return contrasena;
+    }
+
+    public Integer getEdad() {
+        return edad;
+    }
+
+    public Integer getCantidad_de_hijos() {
+        return cantidad_de_hijos;
+    }
+
+    public Integer getSaldo() {
+        return saldo;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public void setCedula(Integer cedula) {
+        this.cedula = cedula;
+    }
+
+    public void setContrasena(String contrasena) {
+        this.contrasena = contrasena;
+    }
+
+    public void setEdad(Integer edad) {
+        this.edad = edad;
+    }
+
+    public void setCantidad_de_hijos(Integer cantidad_de_hijos) {
+        this.cantidad_de_hijos = cantidad_de_hijos;
+    }
+
+    public void setSaldo(Integer saldo) {
+        this.saldo = saldo;
+    }
+
+    public Usuario(String nombre, Integer cedula, String contrasena, Integer edad, Integer cantidad_de_hijos, Integer saldo) {
+        this.nombre = nombre;
+        this.cedula = cedula;
+        this.contrasena = contrasena;
+        this.edad = edad;
+        this.cantidad_de_hijos = cantidad_de_hijos;
+        this.saldo = saldo;
+    }
+
+
+
 
 
 
